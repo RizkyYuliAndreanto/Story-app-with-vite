e4cd4ae387f20f8d403b63f0585126e69234bd8e
My-eryry07 <rizkyand995@gmail.com>
2025-06-16 23:34:34 +0700

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/Story-app-with-vite/sw.js', { scope: '/Story-app-with-vite/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
